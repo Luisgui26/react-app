48cd2b8570e67051a8f3cdbe70f29ca69c2d1319
luis <luisguibarreto8@gmail.com>
2023-06-19 21:10:08 -0300

    Adding some personal changes to the project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import { useEffect, useState } from "react";
 import './App.css';
 import SearchIcon from './search.jpg'
 import MovieCard from "./MovieCard";
+import { createContext } from "react";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null)
 
 const API_URL = 'https://www.omdbapi.com?apikey=bb0f8253';
 
 
 
 const App = () => {
+
+  const [theme, setTheme] = useState('dark')
+  const toggleTheme = () => {
+    setTheme((curr) => (curr ==='light' ? 'dark' : 'light'));
+  }
 
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
@@ -24,37 +33,42 @@ const App = () => {
   }, []);
 
   return (
-    <div className="app">
-      <h1>MovieLand</h1>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div className="app" id={theme}>
+        <h1>MovieFinder</h1>
+        <div className='switch'>
+          <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'}/>
+        </div>
 
-      <div className="search">
-        <input 
-          placeholder="Search for Movies"
-          value={searchTerm} 
-          onChange={(e) => setSearchTerm(e.target.value)}
+        <div className="search">
+          <input 
+            placeholder="Search for Movies"
+            value={searchTerm} 
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
+
+          <img
+            src={SearchIcon}
+            alt='search'
+            onClick={() => searchMovies(searchTerm)}
           />
+        </div>
 
-        <img
-          src={SearchIcon}
-          alt='search'
-          onClick={() => searchMovies(searchTerm)}
-        />
-      </div>
-
-      {movies?.length > 0
-        ?(
-          <div className="container">
-            {movies.map((movie) => (
-              <MovieCard movie={movie}/>
-            ))}
-          </div>
-        ) :
-        (
-          <div className="empty">
-            <h2>No movies found</h2>
+        {movies?.length > 0
+          ?(
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie}/>
+              ))}
             </div>
-        )}
-    </div>
+          ) :
+          (
+            <div className="empty">
+              <h2>No movies found</h2>
+              </div>
+          )}
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
